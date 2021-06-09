8c2004d026b583d16b7e6020fc44196b8af3ee49
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2021-06-09 15:28:02 +0700

    chore(release): automatic commit process

--- app/fuck.sh
+++ app/fuck.sh
@@ -1,6 +1,6 @@
 # shellcheck disable=SC1090,SC2148
 
-_myzs:internal:module:initial "$0"
+myzs:module:new "$0"
 
 if _myzs:internal:checker:command-exist "thefuck"; then
   eval "$(thefuck --alias)" # setup thefuck
